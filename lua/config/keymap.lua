93f10846986f4249408bb56ee2484c5fff4a32f4
jrreed <jacob.reed@oracle.com>
2024-01-02 09:54:57 -0500

    Added buffer delete

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -37,6 +37,7 @@ map("n", "[b", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
 map("n", "]b", "<cmd>bnext<cr>", { desc = "Next buffer" })
 map("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
 map("n", "<leader>`", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+map("n", "<leader>bd", "<cmd>bd<cr>", { desc = "Delete current buffer" })
 
 -- Clear search with <esc>
 map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
