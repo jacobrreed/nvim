84124ac45f2a758e2dd83bd9010fe173eda6d2a9
jrreed <jacob.reed@oracle.com>
2024-01-02 15:45:27 -0500

    dracula theme transparent bg for tmux inactive dimming

--- lua/plugins/ui/dracula.lua
+++ lua/plugins/ui/dracula.lua
@@ -3,6 +3,10 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
+    local dracula = require("dracula")
+    dracula.setup({
+      transparent_bg = true,
+    })
     vim.cmd([[colorscheme dracula]])
   end,
 }
