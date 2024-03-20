57fab68c1499f19d387fad827450c4dc10df3f25
jrreed <jacob.reed@oracle.com>
2024-03-20 14:16:57 -0400

    eldritch theme

---
+++ lua/plugins/ui/eldritch.lua
@@ -0,0 +1,21 @@
+return {
+  "eldritch-theme/eldritch-nvim",
+  priority = 1000,
+  opts = {
+    -- Sets transparent background
+    -- If using lualine will also set transparent bg
+    transparent_bg = true,
+  },
+  config = function(_, opts)
+    local eldritch = require("eldritch")
+    eldritch.setup(opts)
+    vim.cmd("colorscheme eldritch")
+
+    -- If using lualine
+    require("lualine").setup({
+      options = {
+        theme = "eldritch",
+      },
+    })
+  end,
+}
