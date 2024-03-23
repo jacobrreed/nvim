05d122ff3f16770675a5a4efd57b573f58e4a515
jacobrreed <jacob.russell.reed@gmail.com>
2024-03-23 09:35:43 -0500

    floatnote

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -105,34 +105,3 @@ end)
 
 map({ "n" }, "<Backspace>", "^", { desc = "Move to first non-blank character" })
 
-local open_todo_float = function()
-  local gwidth = vim.api.nvim_list_uis()[1].width
-  local gheight = vim.api.nvim_list_uis()[1].height
-  local width = math.floor(gwidth * 0.8)
-  local height = math.floor(gheight * 0.8)
-  local buf
-  local win
-  buf = vim.api.nvim_create_buf(false, true)
-
-  win = vim.api.nvim_open_win(buf, true, {
-    relative = "editor",
-    width = width,
-    height = height,
-    row = (gheight - height) / 2,
-    col = (gwidth - width) / 2,
-    style = "minimal",
-    border = "single",
-  })
-  vim.cmd.edit("~/dev/notes/todo.md")
-
-  vim.opt.number = true
-  vim.opt.relativenumber = true
-
-  vim.keymap.set("n", "q", function()
-    vim.cmd("w")
-    vim.api.nvim_win_close(win, true)
-    vim.api.nvim_buf_delete(buf, { force = true })
-  end, { buffer = buf })
-end
-
-vim.keymap.set("n", "<leader>o", open_todo_float, {})
---
+++ lua/plugins/ui/floatnote.lua
@@ -0,0 +1,6 @@
+return {
+  "jacobrreed/floatnote.nvim",
+  keys = {
+    { "<leader>o", "<cmd>lua require('floatnote').toggle()<cr>", desc = "Toggle TODO" },
+  },
+}
