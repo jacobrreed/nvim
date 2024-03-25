e27c0244247365ce6b0514866a74c2e4e9f6fa78
jrreed <jacob.reed@oracle.com>
2024-03-25 12:47:35 -0400

    note float with input

--- lua/plugins/ui/whichkey.lua
+++ lua/plugins/ui/whichkey.lua
@@ -26,6 +26,9 @@ return {
               name = "Lspsaga",
             },
           },
+          o = {
+            name = "TODO",
+          },
           c = {
             name = "Code",
             n = {
