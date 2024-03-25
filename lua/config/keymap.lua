d8fd26a7572763d568423b22ac4fb787b783ede9
jrreed <jacob.reed@oracle.com>
2024-03-25 12:31:22 -0400

    note float

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -104,3 +104,36 @@ vim.keymap.set("v", "<C-r>", function()
 end)
 
 map({ "n" }, "<Backspace>", "^", { desc = "Move to first non-blank character" })
+
+local gwidth = vim.api.nvim_list_uis()[1].width
+local gheight = vim.api.nvim_list_uis()[1].height
+local width = math.floor(gwidth * 0.8)
+local height = math.floor(gheight * 0.8)
+local buf
+local win
+local open_todo_float = function()
+  buf = vim.api.nvim_create_buf(false, true)
+  win = vim.api.nvim_open_win(buf, true, {
+    relative = "editor",
+    width = width,
+    height = height,
+    row = math.floor((gheight - height) / 2),
+    col = math.floor((gwidth - width) / 2),
+    style = "minimal",
+    border = "rounded",
+  })
+  vim.cmd.edit("~/dev/notes/TODO.md")
+
+  vim.keymap.set("n", "q", function()
+    vim.cmd("w")
+    vim.api.nvim_win_close(win, true)
+    vim.api.nvim_buf_delete(buf, { force = true })
+  end, { buffer = buf })
+  vim.keymap.set("n", "<Esc>", function()
+    vim.cmd("w")
+    vim.api.nvim_win_close(win, true)
+    vim.api.nvim_buf_delete(buf, { force = true })
+  end, { buffer = buf })
+end
+
+vim.keymap.set("n", "<leader>o", open_todo_float, {})
