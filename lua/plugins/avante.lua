daa71cbafc9bcf2a2928739f441a3d35d5027517
jacobrreed <jacob.russell.reed@gmail.com>
2025-01-02 10:42:48 -0500

    copilot with avante

---
+++ lua/plugins/avante.lua
@@ -0,0 +1,49 @@
+return {
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  lazy = false,
+  version = false,
+  opts = {
+    provider = "copilot",
+    auto_suggestions_provider = "copilot",
+    windows = {
+      width = 40,
+      input = {
+        height = 10,
+      },
+      edit = {
+        start_insert = false,
+      },
+      ask = {
+        start_insert = false,
+      },
+    },
+  },
+  build = "make",
+  dependencies = {
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    {
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          use_absolute_path = true,
+        },
+      },
+    },
+    {
+      "MeanderingProgrammer/render-markdown.nvim",
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+  },
+}
