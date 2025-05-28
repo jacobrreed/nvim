23f6cd2fcdb96bc3db410bc7bb44b8c6b39c19b5
jrreed <jacob.reed@oracle.com>
2025-05-28 15:52:22 -0400

    upd

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -40,7 +40,16 @@ return {
     end
   end,
   opts = {
-    ui = { enable = false },
+    ui = {
+      enable = false,
+      checkboxes = {
+        [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
+        ["x"] = { char = "", hl_group = "ObsidianDone" },
+        ["!"] = { char = "", hl_group = "ObsidianImportant" },
+        [">"] = { char = "", hl_group = "ObsidianRightArrow" },
+        ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
+      },
+    },
     workspaces = {
       {
         name = "vault",
