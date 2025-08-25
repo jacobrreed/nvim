bc01a2332afee79845401302fa0987da7d99296e
jrreed <jacob.reed@oracle.com>
2025-08-25 09:16:50 -0400

    upd

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,11 +1,11 @@
 return {
   {
     "zbirenbaum/copilot.lua",
-    dir = "~/dev/copilot.lua",
+    -- dir = "~/dev/copilot.lua",
     event = "InsertEnter",
     opts = {
       filetypes = { markdown = false, help = false },
-      node_command = vim.fn.expand("$FNM_DIR") .. "/node-versions/v22.13.0/installation/bin/node",
+      copilot_node_command = vim.fn.expand("$FNM_DIR") .. "/node-versions/v22.13.0/installation/bin/node",
     },
   },
   {
